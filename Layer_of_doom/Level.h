d526d03d814d66b5ba03fba336e9638be8365791
Matt110110 <matruprasadchand@gmail.com>
2018-06-10 19:21:14 +0530

    Access specifiers updated and motion fucntional

--- Layer_of_doom/Level.h
+++ Layer_of_doom/Level.h
@@ -8,14 +8,14 @@ using namespace std;
 class Level
 {
 	vector<string> _levelData;
+	char getTile(int x, int y);
+	void setTile(int x, int y, char tile);
+	void movePlayer(char tile, int x, int y, Player & player);
 public:
 	Level();
 	void loadLevel(string fileName, Player &player);
 	void printLevel();
-	void setTile(int x, int y, char tile);
 	void tryMovePlayer(char dir, Player &player);
-	char getTile(int x, int y);
-	void movePlayer(char tile, int x, int y, Player & player);
 	~Level();
 };
 
