7306d23c10c1c87961aaebf9881eac63b6370bec
Matt110110 <matruprasadchand@gmail.com>
2018-06-10 14:42:13 +0530

    player class made

---
+++ Layer_of_doom/Player.h
@@ -0,0 +1,18 @@
+#pragma once
+class Player
+{
+	int _level;
+	int _health;
+	int _attack;
+	int _defence;
+	int _experience;
+	int _x;
+	int _y;
+public:
+	Player();
+	void init(int level, int health, int attack, int defence, int experience);
+	void setPosition(int x, int y);
+	void getPosition(int &x, int &y);
+	~Player();
+};
+
---
+++ Layer_of_doom/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	// End of main
+	system("PAUSE");
+	return 0;
+}
