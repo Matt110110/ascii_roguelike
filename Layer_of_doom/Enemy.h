84afc1fa200e1e956560e13ea654eb8bde331cb8
Matt110110 <matruprasadchand@gmail.com>
2018-06-11 23:17:31 +0530

    Enemy class created

---
+++ Layer_of_doom/Enemy.h
@@ -0,0 +1,9 @@
+#pragma once
+class Enemy
+{
+	int _attack, _defence, _hp, _expVal, _level;
+public:
+	Enemy();
+	~Enemy();
+};
+
--- Layer_of_doom/GameSystem.h
+++ Layer_of_doom/GameSystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Player.h"
 #include "Level.h"
+#include "Enemy.h"
 #include <string>
 
 using namespace std;
@@ -8,6 +9,7 @@ class GameSystem
 {
 	Player _player;
 	Level _level;
+	vector<Enemy> _enemies;
 	void _playerMovement();
 public:
 	GameSystem(string levelFile);
